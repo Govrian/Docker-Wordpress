ec3f2bc47896298bac3d5c84d3c487e95ebb6315
Govrian <89915332+Govrian@users.noreply.github.com>
2023-04-27 19:21:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
+set -e
 
+# Exemple de commande à exécuter avant le démarrage du serveur Apache
+echo "Exécution d'une commande personnalisée avant le démarrage d'Apache..."
+
+# Démarrage du serveur Apache en arrière-plan
+apache2-foreground &
+pid="$!"
+
+# Attente de l'arrêt du serveur Apache
+wait "$pid"
